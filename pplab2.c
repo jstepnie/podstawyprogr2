cb5852789f06cfb41771eac021664a159dbe71bf
jstepnie <92805992+jstepnie@users.noreply.github.com>
2022-03-23 23:02:00 +0100

    Update pplab2.c

--- pplab2.c
+++ pplab2.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -33,6 +32,83 @@ int one_two(int *ar1, int le1, int *ar2, int le2, int *ar3) {
                 if (ar2[i2] == ar3[i])
                     n++;
             }
+            if (n == 0)
+                ar3[i3] = ar2[i2++], length++;
+            else
+                i2++;
+                ar3[i3] = ar3[i3];
+        }
+        else {
+            for (int i = 0; ar3[i]; i++) {
+                n = 0;
+                if (ar1[i1] == ar3[i])
+                    n++;
+            }
+            if (n == 0)
+                ar3[i3] = ar1[i1++], length++;
+            else
+                i1++;
+                ar3[i3] = ar3[i3];
+        }
+    }
+    for ( int j = 0; j < length; j++){
+        printf("%d ", ar3[j]);
+    }
+    printf("\n%d ", length);
+    return length;
+
+
+}
+void randomfill(int *arr, int len, int max, int r);
+void statfinder(int *arr, int len);
+
+int main() {
+    srand(time(NULL));
+    int t1[30];
+    int t2[35];
+    int T1[5] = {2, 3, 4, 5, 6};
+    int T2[7] = {1,  7, 8, 9, 10, 11, 12};
+    int T3[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int T[N];
+    int Ts[N];
+    randomfill(T, N, 20, 10);
+    for (int i = 0; i < N; i++) {
+        Ts[i] = T[i];
+        printf("%d ", Ts[i]);
+    }
+    printf("\n");
+    statfinder(Ts, N);
+//    randomfill(t1, 30, 10, 1);
+//    randomfill(t2, 35, 10, 1);
+//    for (int i = 0; i < 30; i++)
+//        printf("%d ", t1[i]);
+//    printf("\n");
+//    for (int i = 0; i < 35; i++)
+//        printf("%d ", t2[i]);
+    printf("\n");
+    one_two(T1, 5, T2, 7, T3);
+}
+
+void randomfill(int *arr, int len, int max, int r) {
+    for (int i = 0; i < len; i++)
+        arr[i] = (rand() % max) + 1 - r;
+}
+
+void  statfinder(int *arr, int len) {
+    for (int i = 0; i < len; i++){
+        float ni = 1;
+        printf("%d ", arr[i]);
+        for (int j = 0; j < len; j++) {
+            if (i != j ) {
+                if (arr[i] == arr[j])
+                    ni++;
+
+            }
+        }
+        float percent = (ni / len) * 100;
+        printf(" wystepuje %f % \n", percent);
+    }
+}
             if (n == 0)
                 ar3[i3] = ar2[i2++], length++;
             else
