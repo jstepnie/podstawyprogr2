559afdcff26076aca089f988fcd53696d3b22ac9
jstepnie <jstepnie@student.agh.edu.pl>
2022-03-24 13:08:42 +0100

    did 2nd task

--- main.c
+++ main.c
@@ -5,107 +5,74 @@
 #define N2 35
 #define N 15
 
-void reverse(int arr[], int len) {
-    len--;
-    for (int i = 0; i < len / 2; i++) {
-        int t = arr[len - i];
-        arr[len - i] = arr[i];
-        arr[i] = t;
-    }
-}
-void dump_array(int *arr, int len) {
-    for (int i = 0; i < len; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-int one_two(int *ar1, int le1, int *ar2, int le2, int *ar3) {
-    int le3 = le1 + le2;
-    if (le3 == 0)
-        return 0;
-
-    int i1 = 0, i2 = 0, length = 0;
-    for (int i3 = 0; i1 + i2 < le3 ; i3++){
-        int n = 0;
-        if (ar1[i1] > ar2[i2]) {
-            n = 0;
-            for (int i = 0; ar3[i]; i++) {
-                if (ar2[i2] == ar3[i])
-                    n++;
-            }
-            if (n == 0)
-                ar3[i3] = ar2[i2++], length++;
-            else
-                i2++;
-                ar3[i3] = ar3[i3];
-        }
-        else {
-            for (int i = 0; ar3[i]; i++) {
-                n = 0;
-                if (ar1[i1] == ar3[i])
-                    n++;
-            }
-            if (n == 0)
-                ar3[i3] = ar1[i1++], length++;
-            else
-                i1++;
-                ar3[i3] = ar3[i3];
-        }
-    }
-    for ( int j = 0; j < length; j++){
-        printf("%d ", ar3[j]);
-    }
-    printf("\n%d ", length);
-    return length;
-
-
-}
-void randomfill(int *arr, int len, int max, int r);
-void statfinder(int *arr, int len);
+void matrixsum(int m1[8][8], int m2);
+void matrixmultiplication(int m1[8][8], int m2[8][8]);
 
 int main() {
+    int losowa;
+    int M1[8][8], i1, j1;
+    int M2[8][8], i2, j2;
     srand(time(NULL));
-    int t1[30];
-    int t2[35];
-    int T1[5] = {2, 3, 4, 5, 6};
-    int T2[7] = {1,  7, 8, 9, 10, 11, 12};
-    int T3[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int T[N];
-    int Ts[N];
-    randomfill(T, N, 20, 10);
-    for (int i = 0; i < N; i++) {
-        Ts[i] = T[i];
-        printf("%d ", Ts[i]);
-    }
-    printf("\n");
-    statfinder(Ts, N);
-//    randomfill(t1, 30, 10, 1);
-//    randomfill(t2, 35, 10, 1);
-//    for (int i = 0; i < 30; i++)
-//        printf("%d ", t1[i]);
-//    printf("\n");
-//    for (int i = 0; i < 35; i++)
-//        printf("%d ", t2[i]);
-    printf("\n");
-    one_two(T1, 5, T2, 7, T3);
-}
-
-void randomfill(int *arr, int len, int max, int r) {
-    for (int i = 0; i < len; i++)
-        arr[i] = (rand() % max) + 1 - r;
-}
-
-void  statfinder(int *arr, int len) {
-    for (int i = 0; i < len; i++){
-        float ni = 1;
-        printf("%d ", arr[i]);
-        for (int j = 0; j < len; j++) {
-            if (i != j ) {
-                if (arr[i] == arr[j])
-                    ni++;
-
-            }
+    for (i1 = 0; i1 < 8; i1++) {
+        for (j1 = 0; j1 < 8; j1++) {
+            losowa = rand() % 15;
+            M1[i1][j1] = losowa;
+            printf("%d ", M1[i1][j1]);
         }
-        float percent = (ni / len) * 100;
-        printf(" wystepuje %f % \n", percent);
+        printf("\n");
     }
+    printf("\n");
+    for (i2 = 0; i2 < 8; i2++) {
+        for (j2 = 0; j2 < 8; j2++) {
+            losowa = rand() % 15;
+            M2[i2][j2] = losowa;
+            printf("%d ", M2[i2][j2]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    matrixsum(M1, M2);
+    printf("\n");
+    matrixmultiplication(M1, M2);
+}
+
+void matrixmultiplication(int m1[8][8], int m2[8][8]){
+    int multiplication = 0;
+    int rowColumnSUM = 0;
+    for (int i1 = 0; i1 < 8; i1++) {
+        int i2 = 0;
+        for ( int j1 = 0; j1 < 8; j1++) {
+            rowColumnSUM =  rowColumnSUM + (m1[i1][j1] * m2[i2++][j1]);
+        }
+        printf("\n");
+        multiplication = multiplication + rowColumnSUM;
+    }
+    printf(" wunik mnozenia wynosi %d", multiplication);
+}
+void matrixsum(int m1[8][8], int m2) {
+    int diagsum = 0;
+    int rowsum = 0;
+    int columnsum = 0;
+    int sum = 0;
+    int i3 = 0, j3 = 0;
+    for (int i=0; i < 8; i++){
+        int j = 0;
+        for (j = 0; j < 8; j++){
+            rowsum = rowsum + m1[i][j];
+            if ( i == j)
+                diagsum = diagsum + m1[i][j];
+            while (i == 0) {
+                columnsum = m1[i3][j] + m1[i3 + 1][j] + m1[i3 + 2][j] + m1[i3 + 3][j] + m1[i3 + 4][j] + m1[i3 + 5][j] +
+                            m1[i3 + 6][j] + m1[i3 + 7][j];
+                printf("|suma w kolumnie %d wynosi %d\n", j + 1, columnsum);
+                break;
+            }
+
+        }
+        printf("-->suma w wierszu %d wynosi %d \n",i + 1, rowsum);
+        sum = sum + rowsum;
+    }
+    printf("suma po przekatnej wynosi %d\n ", diagsum);
+    printf("suma wynosi %d \n", sum);
+
 }
